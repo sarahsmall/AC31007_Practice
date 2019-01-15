4eb5b27008404a7c3addad8b4e8717fcfeaa128a
sarahsmall <sarahsmall@labpc052.computing.dundee.ac.uk>
2019-01-15 14:31:33 +0000

    second commit

--- src/javaapp/JavaApp.java
+++ src/javaapp/JavaApp.java
@@ -17,6 +17,7 @@ public class JavaApp {
     public static void main(String[] args) {
         
         System.out.println("hello world!!!");
+        System.out.println("hi");
         
         // TODO code application logic here
     }
